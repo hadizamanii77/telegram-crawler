b78e14555ea4c29e2cb4e06cd8a35b11eed0cd87
hadi <hadiza1231377@gmail.com>
2020-03-08 08:46:57 -0400

    add connection

---
+++ global_utils/connect.py
@@ -0,0 +1,34 @@
+import asyncio
+import configparser
+import json
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+
+
+class TelegramConnection:
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+
+        self.api_id = config['Telegram']['api_id']
+        self.api_hash = str(config['Telegram']['api_hash'])
+
+        self.phone = config['Telegram']['phone']
+        self.username = config['Telegram']['username']
+
+    def set_credtional(self, api_id, api_hash, phone, username):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.phone = phone
+        self.username = username
+
+    def get_client(self):
+        try:
+            client = TelegramClient(self.username, api_id=self.api_id, api_hash=self.api_hash)
+            client.start()
+            return client
+        except Exception as e:
+            print(e)
+            return None
