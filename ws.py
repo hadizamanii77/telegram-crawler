53ccc381c9299f4859602ae1a5da7d4ded04c09f
hadi <hadiza1231377@gmail.com>
2020-04-08 14:41:10 -0400

    add try catch for ws

--- ws.py
+++ ws.py
@@ -15,14 +15,15 @@ async def gather_posts_api():
                     channel_addr))
             finish_date_time = await websocket.receive()
             await websocket.send("start fetching {} channel".format(channel_addr))
-            await gather_posts(channel_addr,finish_date_time)
+            await gather_posts(channel_addr, finish_date_time)
             await websocket.send("finish fetching {} channel".format(channel_addr))
             await websocket.send(
                 "address : http://194.5.192.130/telegram_files/{date}/{channel_addr}.csv".format(
                     date=str(datetime.date(datetime.now())),
-                    channel_addr=channel_addr.split('/')[-1]))  # this address should get from MessageSaver in next version
+                    channel_addr=channel_addr.split('/')[
+                        -1]))  # this address should get from MessageSaver in next version
         except Exception as e:
             print(e)
-            websocket.send(
+            await websocket.send(
                 "exist one exception"
             )
